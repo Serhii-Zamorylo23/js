3b5a69cce4c26dd98e3e5c333cc0f380a5e86b6a
Сергій Заморило <zamorylo@i.ua>
2023-09-25 19:08:24 +0300

    и

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Не рухай сказав!")
+console.log("some text")
+let Name = "Serhii"
+let Age = 16
+let number = 09752643345
+console.log (`Hello, ${Name} Your age ${Age}, You  number: ${number}`)
