6c57dd2935f513b84c0a98cb8f4fe9d7cdbec319
Сергій Заморило <zamorylo@i.ua>
2023-09-25 22:07:54 +0300

    b

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-alert("Не рухай сказав!")
-console.log("some text")
-let Name = "Serhii"
-let Age = 16
-let number = 09752643345
-console.log (`Hello, ${Name} Your age ${Age}, You  number: ${number}`)
+let win = 0
+let loss = 0
+if (pass == User_Password){
+    alert
+}
