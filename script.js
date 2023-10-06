853ca1e767207e3bf76ba3c14d2d265999da4018
Сергій Заморило <zamorylo@i.ua>
2023-10-06 15:41:08 +0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let win = 0
-let loss = 0
-const answer = prompt("Коли в мене день народження?")
-if (answer = Lus){
-    console.log("Ви виграли!");
-    win = win + 1
+let sum = 0
+let n = prompt('Введіть к-сть разів')
+let max = prompt('Введіть максимальне число')
+for (let i= 1; i <= n; i++){
+    let temp = Math.random()*max
+    temp = Math.floor(temp)
+    console.log(temp)
+    sum+=temp
 }
+console.log(sum)
