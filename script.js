7e0b436fa889d51b46bd9787e3fe4e86bab0c73c
Сергій Заморило <zamorylo@i.ua>
2023-10-16 18:27:16 +0300

    м

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const use= ()=>{
+/*const use= ()=>{
     console.log("Hello")
 }
 use()
 for(let i=1;i<=10;i++){
     use()
 }
+*/
+let sum=0
+const products = [
+    { 
+        id: 1, 
+        name: "Iphone", 
+        price: 3000,
+        count: 2
+    }, 
+    {
+        id: 2,
+        name: "TV",
+        price: 5000,
+        count: 1
+    }
+]
+
+const totalSum = (productsArr) => { 
+    let sum = 0
+    for (let i=0;i<=productsArr.length-1;i++){
+        sum += productsArr[i].price*productsArr[i].count 
+    }
+    return(sum)
+}
+console.log(totalSum(products))
